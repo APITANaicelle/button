dec683cc24105b56d630bddeceab501180f927b0
APITANaicelle <aicelleapitan21@gmail.com>
2024-11-22 01:15:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function changeToBlue() {
+    document.body.style.backgroundColor = "blue";
+    document.body.style.fontFamily = "'Roboto Condensed'";
+    document.body.style.fontSize = "18px";
+
+}
+function changeToPink() {
+    document.body.style.backgroundColor = "pink";   
+    document.body.style.fontFamily = "'Lucida Calligraphy', cursive";
+    document.body.style.fontSize = "18px";
+
+}
+function changeToBrown() {
+    document.body.style.backgroundColor = "brown";
+    document.body.style.fontSize = "40px";
+    document.body.style.fontFamily = "Georgia', serif";
+
+}
